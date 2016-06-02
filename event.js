1c9378d9e18ff93c017e057a81b6ec9790418cea
liya3719 <liya3719@gmail.com>
2016-06-02 14:57:27 +0800

    重写addEvent

--- event.js
+++ event.js
@@ -85,4 +85,16 @@ function unbind(ele,eType,fn){
 		}		
 	}		
 }
-
+//惰性加载函数重写addEvent
+var addEvent = function(elem, type, handler){
+	if( window.addEventListener ){
+		addEvent = function(elem, type, handler){
+			elem.addEventListener(type, handler, false);
+		}
+	}else if( window.attachEvent) {
+		addEvent = function(elem, type, handler) {
+			elem.attachEvent('on'+type, handler);
+		}
+	}
+	addEvent(elem, type, handler)
+}
